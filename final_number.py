510db436a7268929ddf78986c1df7b2cf2667d72
louis8598 <54024319+louis8598@users.noreply.github.com>
2020-03-22 22:37:38 +0800

    增加玩家自訂

--- final_number.py
+++ final_number.py
@@ -3,12 +3,18 @@
 #讓使用者重複去猜
 #猜對的話印出答對了
 #猜錯的話要告訴他比答案大或比答案小
+#減少重複性
+
 import random
-r = random.randint(1, 100)
+start = input('請輸入數字範圍開始值') #讓使用者決定數字範圍開頭
+end = input('請輸入數字範圍結束值') #讓使用者決定數字範圍尾巴
+start = int(start)
+end = int(end)
+r = random.randint(start, end) #將頭跟尾替換成使用者自訂值
 count = 0
 while True:
     count += 1 #count = count + 1
-    k = input('請從1-100猜中一個數字:')
+    k = input('請猜一個數字:')
     k = int(k)
     if k == r:
         print('正確答案!!')
@@ -18,4 +24,4 @@ while True:
         print('比答案大')
     elif k < r:
         print('比答案小')
-    print('這是你猜的第', count,'次')
+    print('這是你猜的第', count,'次')#減少重複性
