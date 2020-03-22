b5cab7f051796e56fcf13680a433b324151d92c3
louis8598 <54024319+louis8598@users.noreply.github.com>
2020-03-22 20:33:38 +0800

    增加計數功能

--- final_number.py
+++ final_number.py
@@ -5,13 +5,17 @@
 #猜錯的話要告訴他比答案大或比答案小
 import random
 r = random.randint(1, 100)
+count = 0
 while True:
+    count += 1 #count = count + 1
     k = input('請從1-100猜中一個數字:')
     k = int(k)
     if k == r:
         print('正確答案!!')
+        print('你總共猜了', count,'次')
         break
     elif k > r:
         print('比答案大')
     elif k < r:
         print('比答案小')
+    print('這是你猜的第', count,'次')
